b44a2b3f4fda6e7465e759a712c9ea7421c98e1d
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2016-12-22 02:13:34 -0200

    Embeds the products_ids into the user serialiser and fetches the correct products from the index action endpoint

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -26,20 +26,46 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
      get :index, format: :json
    end
 
-   it "returns 4 records from the database" do
-   	products_response = json_response[:products]
-   	expect(products_response.length).to eq(4)
-   end
+    context "when is not receiving any product_ids parameter" do
+      before(:each) do
+        get :index, format: :json
+      end
 
-   it "returns the user object into each product" do
-     products_response = json_response[:products]
-     products_response.each do |product_response|
-      expect(product_response[:user]).to be_present
-     end
-   end
+      it "returns 4 records from the database" do
+        products_response = json_response[:products]
+        expect(products_response.length).to eq(4)
+      end
 
-   it { should respond_with 200 }
+      it "returns the user object into each product" do
+        products_response = json_response[:products]
+        products_response.each do |product_response|
+          expect(product_response[:user]).to be_present
+        end
+       end
+
+      it { should respond_with 200 }
+
+    end
+
+    context "when product_ids parameter is sent" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        3.times { FactoryGirl.create :product, user: @user }
+        get :index, params: { product_ids: @user.product_ids }, format: :json
+      end
+
+      it "returns just the products that belong to the user" do
+        products_response = json_response[:products]
+        products_response.each do |product_response|
+        expect(product_response[:user][:email]).to eq @user.email
+      end
+    end
+ end
+
+
   end
+
+
 
   describe "POST #create" do
     context "when is successfully created" do
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -52,6 +52,11 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       expect(user_response[:email]).to eql @user.email
     end
 
+    it "has the product ids as an embeded object" do
+      user_response = json_response[:user]
+      expect(user_response[:product_ids]).to eq []
+    end
+
     it { should respond_with 200 }
   end
 
