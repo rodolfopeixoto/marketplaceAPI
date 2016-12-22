760649a89a3ce6b7b1e1e46eba8a3d741b2e2c01
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2016-12-22 19:22:31 -0200

    Allows the order to be placed along with product quantity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161222051820) do
+ActiveRecord::Schema.define(version: 20161222205705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20161222051820) do
     t.integer  "user_id"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "quantity",   default: 0
     t.index ["user_id"], name: "index_products_on_user_id", using: :btree
   end
 
