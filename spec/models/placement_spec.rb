92505b9bf24885f793e17709c1275e323a16a66b
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2016-12-22 19:38:13 -0200

    Adds quantity to placements

--- spec/models/placement_spec.rb
+++ spec/models/placement_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Placement, type: :model do
 
   it { should respond_to :order_id }
   it { should respond_to :product_id }
+  it { should respond_to :quantity}
 
   describe "associations" do
     it { should belong_to :order }
