ee4940cd21eb6bcd1a238d39c4afeb5b5c2bcfb7
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2016-12-22 02:39:27 -0200

    Adds search scopes on the product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,20 @@ class Product < ApplicationRecord
                     presence: true
 
   belongs_to :user
+
+  scope :filter_by_title, lambda { |keyword|
+    where("lower(title) LIKE ?", "%#{keyword.downcase}%")
+  }
+
+  scope :above_or_equal_to_price, lambda { |price|
+    where("price >= ?", price)
+  }
+
+  scope :bellow_or_equal_to_price, lambda { |price|
+    where("price <= ?", price)
+  }
+
+  scope :recent, -> {
+  	order(:updated_at)
+  }
 end
