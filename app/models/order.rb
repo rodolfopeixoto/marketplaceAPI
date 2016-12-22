ca8aa6104f8ccd8b2e2bb627457799dd21559f2d
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2016-12-22 13:59:52 -0200

    Adds the created method for the orders controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,8 +3,15 @@ class Order < ApplicationRecord
   has_many :placements
   has_many :products, through: :placements
 
-  validates :total, presence: true,
-                      numericality: { greater_than_or_equal_to: 0 }
+  validates :total, numericality: { greater_than_or_equal_to: 0 }
+  # validates :total, presence: true,
+  #                   numericality: { greater_than_or_equal_to: 0 }
 
   validates :user_id, presence: true
+
+  before_validation :set_total!
+
+  def set_total!
+    self.total = products.map(&:price).sum
+  end
 end
--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe Api::V1::OrdersController, type: :controller do
     end
 
     it "returns 4 order records from the user" do
-      puts "Aqui estou eu"
       orders_response = json_response[:orders]
       expect(orders_response.length).to eq(4)
     end
@@ -34,5 +33,23 @@ RSpec.describe Api::V1::OrdersController, type: :controller do
     it { should respond_with 200 }
   end
 
+  describe "POST #create" do
+    before(:each) do
+      current_user = FactoryGirl.create :user
+      api_authorization_header current_user.auth_token
+
+      product_1 = FactoryGirl.create :product
+      product_2 = FactoryGirl.create :product
+      order_params = { product_ids: [product_1.id, product_2.id] }
+      post :create, params:{ user_id: current_user.id, order: order_params }, format: :json
+    end
+
+    it "returns the just user order record" do
+      order_response = json_response[:order]
+      expect(order_response[:id]).to be_present
+    end
+
+    it { should respond_with 201 }
+  end
 
 end
